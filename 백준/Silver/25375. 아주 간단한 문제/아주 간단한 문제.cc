3192247bbfddb5fa00003f2c367a30b683e2160c
lickelon <66820000+lickelon@users.noreply.github.com>
2024-08-06 22:54:45 +0900

    [Silver V] Title: 아주 간단한 문제, Time: 44 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/25375. 아주 간단한 문제/아주 간단한 문제.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+#define all(x) (x).begin(), (x).end()
+
+#define INF 0x7FFFFFFF
+
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using pii = pair<int,int>;
+using pll = pair<ll, ll>;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int q;
+    cin >> q;
+    while(q--) {
+        ll a, b;
+        cin >> a >> b;
+        cout << (!(b%a) && b/a > 1) << "\n";
+    }
+
+    return 0;
+}
