efb6ee3410e5fc733aa5929c45a3cb87ad4ee3f8
lickelon <66820000+lickelon@users.noreply.github.com>
2024-08-13 19:45:03 +0900

    [Silver IV] Title: 동가수열 구하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/25184. 동가수열 구하기/동가수열 구하기.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+#define all(x) (x).begin(), (x).end()
+
+#define INF 0x7FFFFFFF
+
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using pii = pair<int,int>;
+using pll = pair<ll, ll>;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int n;
+    cin >> n;
+    if(n%2) cout << n<< " ";
+    for(int i = n/2; i >= 1; i--) {
+        cout << i << " " << i + n/2 << " ";
+    }
+
+    return 0;
+}
