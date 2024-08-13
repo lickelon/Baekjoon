886c530ab5c7912c835c9cb8fdbc8c87f72cc38f
lickelon <66820000+lickelon@users.noreply.github.com>
2024-08-13 19:29:22 +0900

    [Bronze I] Title: 경우의 수의 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30189. 경우의 수의 합/경우의 수의 합.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+#define all(x) (x).begin(), (x).end()
+
+#define INF 0x7FFFFFFF
+
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using pii = pair<int,int>;
+using pll = pair<ll, ll>;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int n, m;
+    cin >> n >> m;
+
+    cout << (n+1) * (m+1);
+
+    return 0;
+}
