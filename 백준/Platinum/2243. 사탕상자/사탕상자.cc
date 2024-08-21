a4509553462389444787b8fb78d61ed63afa46f0
lickelon <66820000+lickelon@users.noreply.github.com>
2024-08-21 23:34:06 +0900

    [Platinum V] Title: 사탕상자, Time: 260 ms, Memory: 17656 KB -BaekjoonHub

---
+++ 백준/Platinum/2243. 사탕상자/사탕상자.cc
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+
+#define all(x) (x).begin(), (x).end()
+
+#define INF 0x7FFFFFFF
+
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using pii = pair<int,int>;
+using pll = pair<ll, ll>;
+
+template<typename T>
+class segTree {
+private:
+    ll n;
+    T id;
+    T(*merge)(T, T);
+    vector<T> tree;
+public:
+    segTree(ll n, T id, T(*merge)(T, T)) {
+        this->n = n;
+        this->id = id;
+        this->merge = merge;
+        tree.resize(n*4);
+    }
+    void update(ll idx, T value) {
+        _update(1, 1, n, idx, value);
+    }
+    T query(ll l, ll r) {
+        return _query(1, 1, n, l, r);
+    }
+private:
+    void _update(int node, int s, int e, int idx, T value) {
+        if(idx < s || e < idx) return;
+
+        if(s == e) {
+            tree[node] = value;
+            return;
+        }
+
+        _update(node*2, s, (s+e)/2, idx, value);
+        _update(node*2+1, (s+e)/2+1, e, idx, value);
+        tree[node] = merge(tree[node*2], tree[node*2+1]);
+    }
+    T _query(int node, int s, int e, int l, int r) {
+        if(l > e || r < s) return id;
+        if(l <= s && e <= r) return tree[node];
+
+        T lq = _query(node*2, s, (s+e)/2, l, r);
+        T rq = _query(node*2+1, (s+e)/2+1, e, l, r);
+        return merge(lq, rq);
+    }
+};
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int q;
+    cin >> q;
+    segTree<int> s(1000000, 0, [](int a, int b){ return a + b; });
+
+    for(int i = 0; i < q; i++) {
+        int a, b, c;
+        cin >> a;
+        if(a == 1) {
+            cin >> b;
+            int lo = 0, hi = 1000001;
+            while(lo < hi) {
+                int mid = (lo+hi)/2;
+                if(s.query(1, mid) < b) lo = mid+1;
+                else hi = mid;
+            }
+            cout << lo << "\n";
+            s.update(lo, s.query(lo,lo)-1);
+        }
+        else {
+            cin >> b >> c;
+            s.update(b, s.query(b,b)+c);
+        }
+    }
+
+    return 0;
+}
