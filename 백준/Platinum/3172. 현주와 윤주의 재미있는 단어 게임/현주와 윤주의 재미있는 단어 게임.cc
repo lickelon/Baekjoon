cf487db114660cc05044066c83f0dcace8a39aa5
lickelon <66820000+lickelon@users.noreply.github.com>
2024-09-28 20:23:09 +0900

    [Platinum IV] Title: 현주와 윤주의 재미있는 단어 게임, Time: 164 ms, Memory: 7516 KB -BaekjoonHub

---
+++ 백준/Platinum/3172. 현주와 윤주의 재미있는 단어 게임/현주와 윤주의 재미있는 단어 게임.cc
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+
+#define all(x) (x).begin(), (x).end()
+
+#define INF 0x7FFFFFFF
+
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using pii = pair<int,int>;
+using pll = pair<ll, ll>;
+
+bool comp(pair<string, int> a, pair<string, int> b) {
+    reverse(all(a.first));
+    reverse(all(b.first));
+    return a.first < b.first;
+}
+
+template<typename T>
+class segTree {
+private:
+    ll n;
+    T id;
+    T(*merge)(T, T);
+    vector<T> tree;
+public:
+    segTree(ll n, T id, T(*merge)(T, T)) {
+        this->n = n;
+        this->id = id;
+        this->merge = merge;
+        tree.resize(n*4);
+    }
+    void update(ll idx, T value) {
+        _update(1, 1, n, idx, value);
+    }
+    T query(ll l, ll r) {
+        return _query(1, 1, n, l, r);
+    }
+private:
+    void _update(int node, int s, int e, int idx, T value) {
+        if(idx < s || e < idx) return;
+
+        if(s == e) {
+            tree[node] = value;
+            return;
+        }
+
+        _update(node*2, s, (s+e)/2, idx, value);
+        _update(node*2+1, (s+e)/2+1, e, idx, value);
+        tree[node] = merge(tree[node*2], tree[node*2+1]);
+    }
+    T _query(int node, int s, int e, int l, int r) {
+        if(l > e || r < s) return id;
+        if(l <= s && e <= r) return tree[node];
+
+        T lq = _query(node*2, s, (s+e)/2, l, r);
+        T rq = _query(node*2+1, (s+e)/2+1, e, l, r);
+        return merge(lq, rq);
+    }
+};
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int n;
+    cin >> n;
+    vector<pair<string, int>> arr(n);
+    for(auto &u : arr) cin >> u.first;
+    sort(all(arr), comp);
+    for(int i = 0; i < n; i++) {
+        arr[i].second = i+1;
+    }
+    sort(all(arr));
+
+    segTree<int> s(n, 0, [](int a, int b){return a+b;});
+
+    ll ans = 0;
+    for(int i = 0; i < n; i++) {
+        ans += s.query(arr[i].second, n);
+        s.update(arr[i].second, 1);
+    }
+    cout << ans;
+
+    return 0;
+}
