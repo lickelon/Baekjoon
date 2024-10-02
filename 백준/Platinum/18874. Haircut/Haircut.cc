cda51b3f7d32fb211cd215bee3e28963a3c6cf3f
lickelon <66820000+lickelon@users.noreply.github.com>
2024-10-02 23:30:50 +0900

    [Platinum IV] Title: Haircut, Time: 68 ms, Memory: 5940 KB -BaekjoonHub

---
+++ 백준/Platinum/18874. Haircut/Haircut.cc
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+
+#define all(x) (x).begin(), (x).end()
+
+#define INF 0x7FFFFFFF
+
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using pii = pair<int,int>;
+using pll = pair<ll, ll>;
+
+template<typename T>
+class segTree {
+private:
+    ll n;
+    T id;
+    T(*merge)(T, T);
+    vector<T> tree;
+public:
+    segTree(ll n, T id, T(*merge)(T, T)) {
+        this->n = n;
+        this->id = id;
+        this->merge = merge;
+        tree.resize(n*4);
+    }
+    void update(ll idx, T value) {
+        _update(1, 1, n, idx, value);
+    }
+    T query(ll l, ll r) {
+        return _query(1, 1, n, l, r);
+    }
+private:
+    void _update(int node, int s, int e, int idx, T value) {
+        if(idx < s || e < idx) return;
+
+        if(s == e) {
+            tree[node] = value;
+            return;
+        }
+
+        _update(node*2, s, (s+e)/2, idx, value);
+        _update(node*2+1, (s+e)/2+1, e, idx, value);
+        tree[node] = merge(tree[node*2], tree[node*2+1]);
+    }
+    T _query(int node, int s, int e, int l, int r) {
+        if(l > e || r < s) return id;
+        if(l <= s && e <= r) return tree[node];
+
+        T lq = _query(node*2, s, (s+e)/2, l, r);
+        T rq = _query(node*2+1, (s+e)/2+1, e, l, r);
+        return merge(lq, rq);
+    }
+};
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int n;
+    cin >> n;
+    vector<ll> sum(n+1);
+    segTree<ll> s(n+1, 0, [](ll a, ll b){return a + b;});  
+    for(int i = 0; i < n; i++) {
+        ll input;
+        cin >> input;
+        input += 1;
+        s.update(input, s.query(input, input)+1);
+        sum[input] += s.query(input+1, n+1);
+    }
+    for(int i = 1; i <= n; i++) {
+        sum[i] += sum[i-1];
+        cout << sum[i-1] << "\n";
+    }
+    return 0;
+}
