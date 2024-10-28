ec9231b0a188859081294566a29637b978950595
lickelon <66820000+lickelon@users.noreply.github.com>
2024-10-28 23:25:50 +0900

    [Platinum III] Title: 접미사 배열 1, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Platinum/13012. 접미사 배열 1/접미사 배열 1.cc
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+
+#define all(x) (x).begin(), (x).end()
+
+#define INF 0x7FFFFFFF
+
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using pii = pair<int,int>;
+using pll = pair<ll, ll>;
+
+vector<int> getsfx(string &s) {
+    int n = s.size();
+    vector<int> sfx(n), g(n+1), ng(n+1);
+    for(int i = 0; i < n; i++) {
+        sfx[i] = i;
+        g[i] = s[i];
+    }
+
+    g[n] = -1;
+    for(int t = 1; t < n; t <<= 1) {
+        auto cmp = [&](int i, int j) {
+            if(g[i] == g[j]) return g[i+t] < g[j+t];
+            else return g[i] < g[j];
+        };
+        sort(all(sfx), cmp);
+        ng[sfx[0]] = 0;
+        ng[n] = -1;
+        for(int i = 1; i < n; i++) {
+            if(cmp(sfx[i-1], sfx[i])) ng[sfx[i]] = ng[sfx[i-1]] + 1;
+            else ng[sfx[i]] = ng[sfx[i-1]];
+        }
+        g = ng;
+    }
+    return sfx;
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    string s;
+    cin >> s;
+    auto src_sfx = getsfx(s);
+    string t;
+    for(int i = 0; i < s.size(); i++) {
+        t = s;
+        if(t[i] == 'a') continue;
+        t[i] -= 1;
+        auto sfx = getsfx(t);
+        if(sfx == src_sfx) {
+            cout << 1;
+            return 0;
+        }
+    }
+    cout << 0;
+
+    return 0;
+}
