30c23f403492fee015ea8afebb3c0fbff1fb0767
lickelon <66820000+lickelon@users.noreply.github.com>
2024-11-07 00:44:48 +0900

    [Platinum III] Title: 열혈강호 4, Time: 724 ms, Memory: 36180 KB -BaekjoonHub

--- 백준/Platinum/11378. 열혈강호 4/열혈강호 4.cc
+++ 백준/Platinum/11378. 열혈강호 4/열혈강호 4.cc
@@ -11,55 +11,72 @@ using ld = long double;
 using pii = pair<int,int>;
 using pll = pair<ll, ll>;
 
-int flow(vector<vector<pii>> &edge, int start, int end) {
-    int n = edge.size();
-    vector<vector<int>> c(n, vector<int>(n));
-    vector<vector<int>> f(n, vector<int>(n));
-    //init
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < edge[i].size(); j++) {
-            int next = edge[i][j].first;
-            c[i][next] = edge[i][j].second;
-        }
+class Flow {
+    vector<vector<int>> edge;
+    vector<vector<int>> f, c;
+    int n;
+public:
+    Flow(int n) {
+        this->n = n;
+        edge.resize(n);
+        f = vector<vector<int>>(n, vector<int>(n));
+        c = vector<vector<int>>(n, vector<int>(n));
     }
-    int ans = 0;
-    while(true) {
-        //BFS
-        vector<int> parent(n, -1);
-        queue<int> _q;
-        parent[start] = start;
-        _q.push(start);
-        while(!_q.empty() && parent[end] == -1) {
-            int curr = _q.front(); _q.pop();
-            for(auto &[u, _] : edge[curr]) {
-                if(c[curr][u] - f[curr][u] > 0 && parent[u] == -1) {
-                    _q.push(u);
-                    parent[u] = curr;
-                    if(u == end) break;
+    int flow(int start, int end) {
+        init();
+        int ans = 0;
+        while(true) {
+            //BFS
+            vector<int> parent(n, -1);
+            queue<int> _q;
+            parent[start] = start;
+            _q.push(start);
+            while(!_q.empty() && parent[end] == -1) {
+                int curr = _q.front(); _q.pop();
+                for(auto u : edge[curr]) {
+                    if(c[curr][u] - f[curr][u] > 0 && parent[u] == -1) {
+                        _q.push(u);
+                        parent[u] = curr;
+                        if(u == end) break;
+                    }
                 }
             }
+            if(parent[end] == -1) break;
+            //check amount
+            int amount = INF;
+            int curr = end;
+            while(curr != start) {
+                int before = parent[curr];
+                amount = min(amount, c[before][curr] - f[before][curr]);
+                curr = before;
+            }
+            //set
+            curr = end;
+            while(curr != start) {
+                int before = parent[curr];
+                f[before][curr] += amount;
+                f[curr][before] -= amount;
+                curr = before;
+            }
+            ans += amount;
         }
-        if(parent[end] == -1) break;
-        //check amount
-        int amount = INF;
-        int curr = end;
-        while(curr != start) {
-            int before = parent[curr];
-            amount = min(amount, c[before][curr] - f[before][curr]);
-            curr = before;
-        }
-        //set
-        curr = end;
-        while(curr != start) {
-            int before = parent[curr];
-            f[before][curr] += amount;
-            f[curr][before] -= amount;
-            curr = before;
-        }
-        ans += amount;
+        return ans;
     }
-    return ans;
-}
+    void add_edge(int x, int y, int c, bool b = 0) {
+        edge[x].push_back(y);
+        edge[y].push_back(x);
+        this->c[x][y] = c;
+        if(b) this->c[x][y] = c; //양방향 그래프
+    }
+private:
+    void init() {
+        for(auto &u : f) {
+            for(auto &v : u) {
+                v = 0;
+            }
+        }
+    }
+};
 
 int main()
 {
@@ -70,27 +87,23 @@ int main()
     cin >> n >> m >> k;
 
     int S = n + m + 3;
-    vector<vector<pii>> edge(S);
-    auto add_edge = [&](int x, int y, int c) {
-        edge[x].emplace_back(y, c);
-        edge[y].emplace_back(x, 0);
-    };
-    add_edge(0, 1, k);
+    Flow f(S);
+    f.add_edge(0, 1, k);
     for(int i = 1; i <= n; i++) {
-        add_edge(0, i+1, 1);
-        add_edge(1, i+1, k);
+        f.add_edge(0, i+1, 1);
+        f.add_edge(1, i+1, k);
         int jobs;
         cin >> jobs;
         for(int j = 0; j < jobs; j++) {
             int input;
             cin >> input;
-            add_edge(i+1, input + (n+1), 1);
+            f.add_edge(i+1, input + (n+1), 1);
         }
     }
     for(int i = 1; i <= m; i++) {
-        add_edge(i + (n+1), S-1, 1);
+        f.add_edge(i + (n+1), S-1, 1);
     }
-    cout << flow(edge, 0, S-1);
+    cout << f.flow(0, S-1);
 
     return 0;
 }
