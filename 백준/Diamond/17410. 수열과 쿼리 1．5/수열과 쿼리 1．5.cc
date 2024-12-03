9e348fb5b1a746620b3012413f3f631ce64ac83f
lickelon <66820000+lickelon@users.noreply.github.com>
2024-12-04 00:25:45 +0900

    [Diamond V] Title: 수열과 쿼리 1.5, Time: 1152 ms, Memory: 3116 KB -BaekjoonHub

---
+++ 백준/Diamond/17410. 수열과 쿼리 1．5/수열과 쿼리 1．5.cc
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+
+#define all(x) (x).begin(), (x).end()
+
+#define INF 0x7FFFFFFF
+
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using pii = pair<int,int>;
+using pll = pair<ll, ll>;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for(auto &e : arr) cin >> e;
+    int s = 2500;
+    vector<vector<int>> bucket(n/s+1);
+    for(int i = 0; i < n; i++) {
+        bucket[i/s].emplace_back(arr[i]);
+    }
+    for(auto &e : bucket) {
+        sort(all(e));
+    }
+
+    int m;
+    cin >> m;
+    for(int i = 0; i < m; i++) {
+        int q;
+        cin >> q;
+        if(q == 2) {
+            int l, r, k;
+            cin >> l >> r >> k;
+            l--, r--;
+            int ans = 0;
+            for(int i = l; i <= r; i++) {
+                if((i-1)/s != (i/s) && i+s-1 <= r) {
+                    int idx = i/s;
+                    ans += bucket[idx].end() - upper_bound(all(bucket[idx]), k);
+                    i += s-1;
+                }
+                else {
+                    ans += (arr[i] > k);
+                }
+            }
+            cout << ans << "\n";
+        }
+        if(q == 1) {
+            int i, k;
+            cin >> i >> k;
+            i--;
+            int idx = i/s;
+            bucket[idx].erase(lower_bound(all(bucket[idx]),arr[i]));
+            arr[i] = k;
+            bucket[idx].insert(lower_bound(all(bucket[idx]),k),k);
+        }
+    }
+
+    return 0;
+}
