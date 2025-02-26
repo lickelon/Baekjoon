6e7483eec8b3e00c7fb03e872d3ab9619f24e5c1
lickelon <66820000+lickelon@users.noreply.github.com>
2025-02-27 00:30:23 +0900

    [Silver II] Title: 썸 팰린드롬, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/25180. 썸 팰린드롬/썸 팰린드롬.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+#define all(x) (x).begin(), (x).end()
+
+#define INF 0x7FFFFFFF
+
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using pii = pair<int,int>;
+using pll = pair<ll, ll>;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int n;
+    cin >> n;
+
+    int c = (n-1)/9+1;
+    if(n % 2 == 1 && c % 2 == 0) c++;
+    cout << c;
+
+    return 0;
+}
