e5cee1b0b8b4932bc45aa7db097eccdf577b4e09
lickelon <66820000+lickelon@users.noreply.github.com>
2025-04-19 22:27:26 +0900

    [Silver IV] Title: Greedy Scheduler, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/16692. Greedy Scheduler/Greedy Scheduler.cc
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+#define all(x) (x).begin(), (x).end()
+
+#define INF 0x7FFFFFFF
+
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using pii = pair<int,int>;
+using pll = pair<ll, ll>;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int n, c;
+    cin >> n >> c;
+    
+    vector<int> arr(n);
+    for(int i = 0; i < c; i++) {
+        int input;
+        cin >> input;
+
+        int m = *min_element(all(arr));
+        for(auto &e : arr) e -= m;
+        for(int i = 0; i < n; i++) {
+            if(arr[i] == 0) {
+                cout << i + 1 << " ";
+                arr[i] = input;
+                break;
+            }
+        }
+    }
+
+    return 0;
+}
