f61540eab5ed438dab97a0b6d7b48eae46024403
lickelon <66820000+lickelon@users.noreply.github.com>
2025-04-22 01:51:33 +0900

    [Silver II] Title: 약수의 합 2, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/17427. 약수의 합 2/약수의 합 2.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+#define all(x) (x).begin(), (x).end()
+
+#define INF 0x7FFFFFFF
+
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using pii = pair<int,int>;
+using pll = pair<ll, ll>;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int n;
+    cin >> n;
+
+    ll ans = 0;
+    for(int i = 1; i <= n; i++) {
+        for(int j = i; j <= n; j += i) {
+            ans += i;
+        }
+    }
+    cout << ans;
+
+    return 0;
+}
