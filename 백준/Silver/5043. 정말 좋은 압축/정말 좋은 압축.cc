44df54f669c95f3fbfdd1e1a9b8e0f93e1263aa1
lickelon <66820000+lickelon@users.noreply.github.com>
2025-04-23 01:06:38 +0900

    [Silver V] Title: 정말 좋은 압축, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/5043. 정말 좋은 압축/정말 좋은 압축.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+#define all(x) (x).begin(), (x).end()
+
+#define INF 0x7FFFFFFF
+
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using pii = pair<int,int>;
+using pll = pair<ll, ll>;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    ll n, b;
+    cin >> n >> b;
+    cout << (((2ll << b) - 1ll) >= n ? "yes" : "no");
+
+    return 0;
+}
