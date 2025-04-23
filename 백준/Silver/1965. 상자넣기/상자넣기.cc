e430eb4bb282b5b263d725283fd517bc3aec16c6
lickelon <66820000+lickelon@users.noreply.github.com>
2025-04-24 01:14:20 +0900

    [Silver II] Title: 상자넣기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1965. 상자넣기/상자넣기.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+#define all(x) (x).begin(), (x).end()
+
+#define INF 0x7FFFFFFF
+
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using pii = pair<int,int>;
+using pll = pair<ll, ll>;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for(auto &e : arr) cin >> e;
+    vector<int> dp(n, 1);
+    for(int i = 1; i < n; i++) {
+        for(int j = 0; j < i; j++) {
+            if(arr[j] < arr[i]) {
+                dp[i] = max(dp[i], dp[j]+1);
+            }
+        }
+    }
+    cout << *max_element(all(dp));
+    return 0;
+}
