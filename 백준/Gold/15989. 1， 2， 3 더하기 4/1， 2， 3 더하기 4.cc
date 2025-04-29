f670e023b5f4e1bc4c8b028ad91c0961ffab6f60
lickelon <66820000+lickelon@users.noreply.github.com>
2025-04-29 22:30:48 +0900

    [Gold V] Title: 1, 2, 3 더하기 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+#define all(x) (x).begin(), (x).end()
+
+#define INF 0x7FFFFFFF
+
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using pii = pair<int,int>;
+using pll = pair<ll, ll>;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int T;
+    cin >> T;
+    while(T--) {
+        int n;
+        cin >> n;
+        cout << (n*(n+6)+12)/12 << "\n";
+    }
+    return 0;
+}
