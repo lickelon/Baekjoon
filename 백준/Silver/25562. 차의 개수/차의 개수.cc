47480afb8913dcd7495a57ec1f99dfe0ae907deb
lickelon <66820000+lickelon@users.noreply.github.com>
2025-05-01 23:30:02 +0900

    [Silver III] Title: 차의 개수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/25562. 차의 개수/차의 개수.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+#define all(x) (x).begin(), (x).end()
+
+#define INF 0x7FFFFFFF
+
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using pii = pair<int,int>;
+using pll = pair<ll, ll>;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int n;
+    cin >> n;
+
+    cout << (n * (n-1)) / 2 << "\n";
+    for(int i = 0; i < n; i++) {
+        cout << (1 << i) << " ";
+    }
+    cout << "\n";
+    
+    cout << n-1 << "\n";
+    for(int i = 0; i < n; i++) {
+        cout << 1 + i << " ";
+    }
+
+    return 0;
+}
