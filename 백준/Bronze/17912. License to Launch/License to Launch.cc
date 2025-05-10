e704dd4508d536d4bf2d3f930a93cbc81d72719a
lickelon <66820000+lickelon@users.noreply.github.com>
2025-05-11 02:32:37 +0900

    [Bronze IV] Title: License to Launch, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Bronze/17912. License to Launch/License to Launch.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+#define all(x) (x).begin(), (x).end()
+
+#define INF 0x3FFFFFFF
+
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using pii = pair<int,int>;
+using pll = pair<ll, ll>;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for(auto &e : arr) cin >> e;
+    cout << min_element(all(arr)) - arr.begin();
+
+    return 0;
+}
