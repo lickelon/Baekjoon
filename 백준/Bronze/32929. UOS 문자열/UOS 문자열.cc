3167ba43e3c1610f41de2abf8f0774184d2fbb30
lickelon <66820000+lickelon@users.noreply.github.com>
2025-05-11 02:17:58 +0900

    [Bronze V] Title: UOS 문자열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32929. UOS 문자열/UOS 문자열.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+#define all(x) (x).begin(), (x).end()
+
+#define INF 0x3FFFFFFF
+
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using pii = pair<int,int>;
+using pll = pair<ll, ll>;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int x;
+    cin >> x;
+    string s = "UOS";
+    cout << s[(x-1)%3];
+
+    return 0;
+}
