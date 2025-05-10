c8454b6ea8b3df56a272f264d9ffed3bfd9a154a
lickelon <66820000+lickelon@users.noreply.github.com>
2025-05-11 02:34:03 +0900

    [Bronze IV] Title: Haughty Cuisine, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/20336. Haughty Cuisine/Haughty Cuisine.cc
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+#define all(x) (x).begin(), (x).end()
+
+#define INF 0x3FFFFFFF
+
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using pii = pair<int,int>;
+using pll = pair<ll, ll>;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int n, m;
+    cin >> n >> m;
+
+    cout << m << "\n";
+    for(int i = 0; i < m; i++) {
+        string s;
+        cin >> s;
+        cout << s << "\n";
+    }
+
+    return 0;
+}
