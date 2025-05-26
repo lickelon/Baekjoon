2c45c0c45938190d5c7973a0b94868dd2d4114cc
lickelon <66820000+lickelon@users.noreply.github.com>
2025-05-26 20:47:46 +0900

    [Bronze III] Title: 3대 512, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/33990. 3대 512/3대 512.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+#define all(x) (x).begin(), (x).end()
+
+#define INF 0x3FFFFFFF
+
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using pii = pair<int,int>;
+using pll = pair<ll, ll>;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int n;
+    cin >> n;
+
+    int ans = INF;
+    for(int i = 0; i < n; i++) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        int sum = a+b+c;
+        if(sum >= 512) ans = min(ans, sum);
+    }
+    cout << (ans == INF ? -1 : ans);
+
+    return 0;
+}
