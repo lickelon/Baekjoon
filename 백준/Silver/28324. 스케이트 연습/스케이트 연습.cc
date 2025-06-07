06e56d917e03c294bc951296b46a41241d03359e
lickelon <66820000+lickelon@users.noreply.github.com>
2025-06-08 04:08:31 +0900

    [Silver IV] Title: 스케이트 연습, Time: 64 ms, Memory: 5928 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/28324. 스케이트 연습/스케이트 연습.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+#define all(x) (x).begin(), (x).end()
+
+#define INF 0x3FFFFFFF
+
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using pii = pair<int,int>;
+using pll = pair<ll, ll>;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int n;
+    cin >> n;
+    vector<ll> arr(n);
+    for(auto &e : arr) cin >> e;
+    reverse(all(arr));
+
+    ll ans = 0;
+    ll curr = 0;
+    for(auto e : arr) {
+        if(curr < e) curr++;
+        else curr = e;
+        ans += curr;
+    }
+    cout << ans;
+
+    return 0;
+}
