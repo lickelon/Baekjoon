9895213f8339587e4ec159d241dd40e1084fd7ba
lickelon <66820000+lickelon@users.noreply.github.com>
2025-06-10 01:24:19 +0900

    [Silver IV] Title: 그거 왜 말해!, Time: 0 ms, Memory: 2912 KB -BaekjoonHub

---
+++ 백준/Silver/33985. 그거 왜 말해！/그거 왜 말해！.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+#define all(x) (x).begin(), (x).end()
+
+#define INF 0x3FFFFFFF
+
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using pii = pair<int,int>;
+using pll = pair<ll, ll>;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+
+    cout << (s[0] == 'A' && s[n-1] == 'B' ? "Yes" : "No");
+
+    return 0;
+}
