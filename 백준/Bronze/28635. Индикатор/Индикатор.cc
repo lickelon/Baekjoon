062fb9b7673f309852c4b2ed6a9f1499ce711b5d
lickelon <jindoman0@naver.com>
2025-07-15 20:53:54 +0900

    [Bronze IV] Title: Индикатор, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/28635. Индикатор/Индикатор.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+#define all(x) (x).begin(), (x).end()
+
+#define INF 0x3FFFFFFF
+
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using pii = pair<int,int>;
+using pll = pair<ll, ll>;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int m, a, b;
+    cin >> m >> a >> b;
+    if(a > b) b += m;
+    cout << b-a;
+
+    return 0;
+}
