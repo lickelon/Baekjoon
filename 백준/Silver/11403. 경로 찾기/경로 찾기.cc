672ccd052bee1c4451869b35f829c121d89820a1
Giryeong Son <jindoman0@naver.com>
2025-07-30 00:22:14 +0900

    [Silver I] Title: 경로 찾기, Time: 0 ms, Memory: 2152 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+#define all(x) (x).begin(), (x).end()
+
+#define INF 0x3FFFFFFF
+
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using pii = pair<int,int>;
+using pll = pair<ll, ll>;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int n;
+    cin >> n;
+    vector<vector<int>> arr(n, vector<int>(n));
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+             cin >> arr[i][j];
+        }
+    }
+
+    for(int k = 0; k < n; k++) {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                arr[i][j] |= arr[i][k] & arr[k][j];
+            }
+        }
+    }
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
