49b9336dfdb77d7762a376b0867d68aa9f8b5a2d
Giryeong Son <jindoman0@naver.com>
2026-03-31 19:32:21 +0900

    [Bronze III] Title: 피막치, Time: 0 ms, Memory: 2084 KB -BaekjoonHub

---
+++ 백준/Bronze/34723. 피막치/피막치.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+#define INF 0x3FFFFFFF
+
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using pii = pair<int,int>;
+using pll = pair<ll, ll>;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int a, b, c, x;
+    cin >> a >> b >> c >> x;
+
+    int ans = INF;
+    for(int i = 1; i <= a; i++) {
+        for(int j = 1; j <= b; j++) {
+            for(int k = 1; k <= c; k++) {
+                ans = min(ans, abs((i+j)*(j+k) - x));
+            }
+        }
+    }
+    cout << ans;
+
+    return 0;
+}
